dedb9e53c4b28ccceb5fbabed4a58bbcd8e8ebb5
Olga Kova <olgavpj@gmail.com>
2021-11-22 13:54:01 +0300

    restore tests as it was before to increase CC maintainability

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,11 +3,6 @@ import { expect, describe, it } from '@jest/globals';
 import genDiff from '../src/index.js';
 import { getFilePath } from '../src/utils.js';
 
-const getExptectedResult = (filename) => {
-  const stylishResultPath = getFilePath(filename);
-  return fs.readFileSync(stylishResultPath, 'utf-8');
-};
-
 describe('genDiffOutput', () => {
   const file1json = getFilePath('file1.json');
   const file2json = getFilePath('file2.json');
@@ -15,22 +10,23 @@ describe('genDiffOutput', () => {
   const file2yml = getFilePath('file2.yml');
 
   it('test stylish formatter', () => {
-    const expectedResultStylish = getExptectedResult('expectedResultStylish.txt');
+    const stylishResultPath = getFilePath('expectedResultStylish.txt');
+    const expectedResultStylish = fs.readFileSync(stylishResultPath, 'utf-8');
 
     expect(genDiff(file1json, file2json, 'stylish')).toBe(expectedResultStylish);
     expect(genDiff(file1yml, file2yml, 'stylish')).toBe(expectedResultStylish);
   });
 
   it('test plain formatter', () => {
-    const expectedResultPlain = getExptectedResult('expectedResultPlain.txt');
-
+    const stylishResultPath = getFilePath('expectedResultPlain.txt');
+    const expectedResultPlain = fs.readFileSync(stylishResultPath, 'utf-8');
     expect(genDiff(file1json, file2json, 'plain')).toBe(expectedResultPlain);
     expect(genDiff(file1yml, file2yml, 'plain')).toBe(expectedResultPlain);
   });
 
   it('test json formatter', () => {
-    const expectedResultJson = getExptectedResult('expectedResultJSON.txt');
-
+    const stylishResultPath = getFilePath('expectedResultJSON.txt');
+    const expectedResultJson = fs.readFileSync(stylishResultPath, 'utf-8');
     expect(genDiff(file1json, file2json, 'json')).toBe(expectedResultJson);
     expect(genDiff(file1yml, file2yml, 'json')).toBe(expectedResultJson);
   });
