fb5d1556d28a9532c4a79ad4296e077d14ae96c1
Olga Kova <olgavpj@gmail.com>
2021-11-22 15:19:19 +0300

    fix plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,34 +12,36 @@ const formatValue = (value) => {
   return value;
 };
 
+const createline = (keys, item, func) => {
+  const { key, type } = item;
+  const path = [...keys, key].join('.');
+  switch (type) {
+    case 'removed':
+      return `Property '${path}' was removed`;
+    case 'added': {
+      const { value } = item;
+      return `Property '${path}' was added with value: ${formatValue(value)}`;
+    }
+    case 'updated': {
+      const { valueBefore, valueAfter } = item;
+      return `Property '${path}' was updated. From ${formatValue(valueBefore)} to ${formatValue(valueAfter)}`;
+    }
+    case 'nested': {
+      const { children } = item;
+      return func(children, [...keys, key]);
+    }
+    case 'unchanged': {
+      return '';
+    }
+    default:
+      throw new Error('This format is not supported.');
+  }
+};
+
 const plain = (diffs) => {
   const iter = (obj, keys) => {
     const result = obj
-      .flatMap((item) => {
-        const { key, type } = item;
-        const path = [...keys, key].join('.');
-        switch (type) {
-          case 'removed':
-            return `Property '${path}' was removed`;
-          case 'added': {
-            const { value } = item;
-            return `Property '${path}' was added with value: ${formatValue(value)}`;
-          }
-          case 'updated': {
-            const { valueBefore, valueAfter } = item;
-            return `Property '${path}' was updated. From ${formatValue(valueBefore)} to ${formatValue(valueAfter)}`;
-          }
-          case 'nested': {
-            const { children } = item;
-            return iter(children, [...keys, key]);
-          }
-          case 'unchanged': {
-            return '';
-          }
-          default:
-            throw new Error('This format is not supported.');
-        }
-      })
+      .flatMap((item) => createline(keys, item, iter))
       .filter((item) => item !== '');
     return result.join('\n');
   };
