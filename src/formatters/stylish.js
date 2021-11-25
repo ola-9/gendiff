4564b5b855bf37d9db9fa67aff7334b3aff360bb
Olga Kova <olgavpj@gmail.com>
2021-11-25 19:19:26 +0300

    adjust stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 const typesOfIndent = {
   added: '+ ',
   removed: '- ',
+  unchanged: '  ',
 };
 
 const createIndent = (depth, replacer = ' ', spacesCount = 4) => {
@@ -12,25 +13,16 @@ const createIndent = (depth, replacer = ' ', spacesCount = 4) => {
 
 const adjustIndent = (indent, type) => `${indent.slice(0, -2)}${typesOfIndent[type]}`;
 
-const stringify = (value, depth) => {
-  const indent = createIndent(depth);
-  const lines = Object.entries(value)
-    .map(([innerKey, innerValue]) => {
-      if (!_.isPlainObject(innerValue)) {
-        return `${indent}${innerKey}: ${innerValue}`;
+const stringify = (currentValue, depth) => {
+  const currentIndent = createIndent(depth);
+  const lines = Object.entries(currentValue)
+    .map(([key, value]) => {
+      if (!_.isPlainObject(value)) {
+        return `${currentIndent}${key}: ${value}`;
       }
-      return [`${indent}${innerKey}: {\n${stringify(innerValue, depth + 1)}\n${indent}}`].join('\n');
+      return `${currentIndent}${key}: {\n${stringify(value, depth + 1)}\n${currentIndent}}`;
     });
-
   return lines.join('\n');
-};
-
-const getValue = (value, depth, indent) => {
-  if (!_.isPlainObject(value)) {
-    return `${value}`;
-  }
-
-  return `{\n${stringify(value, depth + 1)}\n${indent}}`;
 };
 
 const stylish = (diffStructure) => {
@@ -39,29 +31,37 @@ const stylish = (diffStructure) => {
     const lines = diffs
       .map((diff) => {
         const { key, type } = diff;
+
         switch (type) {
-          case 'added': {
+          case 'added':
+          case 'removed':
+          case 'unchanged': {
             const { value } = diff;
-            return `${adjustIndent(indent, type)}${key}: ${getValue(value, depth, indent)}`;
+            if (_.isPlainObject(value)) {
+              return [`${adjustIndent(indent, type)}${key}: {`, `${stringify(value, depth + 1)}\n${indent}}`].join('\n');
+            }
+            return `${adjustIndent(indent, type)}${key}: ${value}`;
           }
-          case 'removed': {
-            const { value } = diff;
-            return `${adjustIndent(indent, type)}${key}: ${getValue(value, depth, indent)}`;
-          }
+
           case 'updated': {
             const { valueBefore, valueAfter } = diff;
-            const lineBefore = `${adjustIndent(indent, 'removed')}${key}: ${getValue(valueBefore, depth, indent)}`;
-            const lineAfter = `${adjustIndent(indent, 'added')}${key}: ${getValue(valueAfter, depth, indent)}`;
+
+            const lineBefore = (_.isPlainObject(valueBefore))
+              ? [`${adjustIndent(indent, 'removed')}${key}: {`, `${stringify(valueBefore, depth + 1)}\n${indent}}`].join('\n')
+              : `${adjustIndent(indent, 'removed')}${key}: ${valueBefore}`;
+
+            const lineAfter = (_.isPlainObject(valueAfter))
+              ? [`${adjustIndent(indent, 'added')}${key}: {`, `${stringify(valueAfter, depth + 1)}\n${indent}}`].join('\n')
+              : `${adjustIndent(indent, 'added')}${key}: ${valueAfter}`;
+
             return `${lineBefore}\n${lineAfter}`;
           }
+
           case 'nested': {
             const { children } = diff;
             return `${indent}${key}: {\n${iter(children, depth + 1)}\n${indent}}`;
           }
-          case 'unchanged': {
-            const { value } = diff;
-            return `${indent}${key}: ${value}`;
-          }
+
           default: {
             throw new Error(`This ${type} is not supported.`);
           }
